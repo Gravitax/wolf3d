6089f45cb4a0cf75dbd4b6c6dfaa91b22e6dd206
Gravitax <marcaboye@gmail.com>
2019-10-21 17:19:20 +0200

    clean

--- SDL2-2.0.10/build/e_log10.d
+++
@@ -1,9 +0,0 @@
-build/e_log10.lo: /home/maboye/42/wolf3d/SDL2-2.0.10/src/libm/e_log10.c \
- /home/maboye/42/wolf3d/SDL2-2.0.10/src/libm/math_libm.h \
- /home/maboye/42/wolf3d/SDL2-2.0.10/src/libm/../SDL_internal.h \
- /home/maboye/42/wolf3d/SDL2-2.0.10/src/libm/../dynapi/SDL_dynapi.h \
- /home/maboye/42/wolf3d/SDL2-2.0.10/src/libm/../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h include/SDL_platform.h include/begin_code.h \
- include/close_code.h \
- /home/maboye/42/wolf3d/SDL2-2.0.10/src/libm/math_private.h \
- include/SDL_endian.h include/SDL_stdinc.h include/SDL_config.h
