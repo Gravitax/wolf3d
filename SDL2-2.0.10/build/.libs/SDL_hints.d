6700988b530803126412a92f3eebbf1f5bf3b179
Gravitax <marcaboye@gmail.com>
2019-10-24 18:29:56 +0200

    clean

--- SDL2-2.0.10/build/.libs/SDL_hints.d
+++
@@ -1,7 +0,0 @@
-build/SDL_hints.lo: /home/maboye/42/wolf3d/SDL2-2.0.10/src/SDL_hints.c \
- /home/maboye/42/wolf3d/SDL2-2.0.10/src/./SDL_internal.h \
- /home/maboye/42/wolf3d/SDL2-2.0.10/src/./dynapi/SDL_dynapi.h \
- /home/maboye/42/wolf3d/SDL2-2.0.10/src/./dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h include/SDL_platform.h include/begin_code.h \
- include/close_code.h include/SDL_hints.h include/SDL_stdinc.h \
- include/SDL_config.h include/SDL_error.h
