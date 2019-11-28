6c68f6ab3ec836cd851e38735a422b4cc0d86b47
Administrator <admin@example.com>
2019-11-28 15:45:37 +0100

    ca  degage

--- SDL2/SDL2-2.0.10/build/.libs/SDL_stdlib.d
+++
@@ -1,9 +0,0 @@
-build/SDL_stdlib.lo: \
- /home/samuel/Documents/SDL2/SDL2-2.0.10/src/stdlib/SDL_stdlib.c \
- /home/samuel/Documents/SDL2/SDL2-2.0.10/src/stdlib/../SDL_internal.h \
- /home/samuel/Documents/SDL2/SDL2-2.0.10/src/stdlib/../dynapi/SDL_dynapi.h \
- /home/samuel/Documents/SDL2/SDL2-2.0.10/src/stdlib/../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h include/SDL_platform.h include/begin_code.h \
- include/close_code.h include/SDL_stdinc.h include/SDL_config.h \
- /home/samuel/Documents/SDL2/SDL2-2.0.10/src/stdlib/../libm/math_libm.h \
- /home/samuel/Documents/SDL2/SDL2-2.0.10/src/stdlib/../libm/../SDL_internal.h
--- SDL2/SDL2-2.0.10/build/SDL_dummysensor.d
+++
@@ -1,8 +0,0 @@
-build/SDL_dummysensor.lo: \
- /home/samuel/Documents/SDL2/SDL2-2.0.10/src/sensor/dummy/SDL_dummysensor.c \
- include/SDL_config.h include/SDL_platform.h include/begin_code.h \
- include/close_code.h include/SDL_error.h include/SDL_stdinc.h \
- include/SDL_config.h include/SDL_sensor.h include/SDL_error.h \
- /home/samuel/Documents/SDL2/SDL2-2.0.10/src/sensor/dummy/SDL_dummysensor.h \
- /home/samuel/Documents/SDL2/SDL2-2.0.10/src/sensor/dummy/../SDL_syssensor.h \
- /home/samuel/Documents/SDL2/SDL2-2.0.10/src/sensor/dummy/../SDL_sensor_c.h
