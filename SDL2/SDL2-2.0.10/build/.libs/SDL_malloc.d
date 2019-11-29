46fc58f572c849f89e4b08ff8f68436667108d74
Samuel NEVEU <saneveu@e3r3p1.42.fr>
2019-11-29 17:53:15 +0100

    clean

--- SDL2/SDL2-2.0.10/build/.libs/SDL_malloc.d
+++
@@ -1,8 +0,0 @@
-build/SDL_malloc.lo: \
- /home/samuel/Documents/SDL2/SDL2-2.0.10/src/stdlib/SDL_malloc.c \
- /home/samuel/Documents/SDL2/SDL2-2.0.10/src/stdlib/../SDL_internal.h \
- /home/samuel/Documents/SDL2/SDL2-2.0.10/src/stdlib/../dynapi/SDL_dynapi.h \
- /home/samuel/Documents/SDL2/SDL2-2.0.10/src/stdlib/../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h include/SDL_platform.h include/begin_code.h \
- include/close_code.h include/SDL_stdinc.h include/SDL_config.h \
- include/SDL_atomic.h include/SDL_stdinc.h include/SDL_error.h
--- SDL2/SDL2-2.0.10/build/.libs/SDL_render_gles.d
+++
@@ -1,7 +0,0 @@
-build/SDL_render_gles.lo: \
- /home/samuel/Documents/SDL2/SDL2-2.0.10/src/render/opengles/SDL_render_gles.c \
- /home/samuel/Documents/SDL2/SDL2-2.0.10/src/render/opengles/../../SDL_internal.h \
- /home/samuel/Documents/SDL2/SDL2-2.0.10/src/render/opengles/../../dynapi/SDL_dynapi.h \
- /home/samuel/Documents/SDL2/SDL2-2.0.10/src/render/opengles/../../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h include/SDL_platform.h include/begin_code.h \
- include/close_code.h
